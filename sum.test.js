e4710bf52951b680e3997d2408d83335153517a1
Pascal Nehlsen <pascal.nehlsen@developerakademie.com>
2026-08-12 18:05:28 +0200

    feat(ci): execute tests

--- sum.test.js
+++ sum.test.js
@@ -3,7 +3,7 @@ const assert = require('node:assert');
 const { sum, average } = require('./sum.js');
 
 test('sum addiert zwei Zahlen', () => {
-  assert.strictEqual(sum(2, 3), 5);
+  assert.strictEquali(sum(2, 3), 5);
 });
 
 test('sum funktioniert mit negativen Zahlen', () => {
@@ -14,6 +14,6 @@ test('average berechnet den Durchschnitt', () => {
   assert.strictEqual(average([2, 4, 6]), 4);
 });
 
-test('average gibt 0 bei leerem Array zurück', () => {
+test('average gibt 0 be leerem Array zurück', () => {
   assert.strictEqual(average([]), 0);
 });
